6877d2be0a14c79ff4e45686d60ff59d1bfb74ac
mehmetsagir <memetsagirint@gmail.com>
2022-05-05 01:11:22 +0300

    add world traveler

---
+++ WorldTraveler/WorldTraveler/WorldTravelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravelerApp.swift
+//  WorldTraveler
+//
+//  Created by Mehmet Sağır on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorldTravelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
