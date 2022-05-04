41f4934dcce89134bb3e232b3302a19fe3fb2aef
mehmetsagir <memetsagirint@gmail.com>
2022-05-05 01:13:51 +0300

    add content view

--- WorldTraveler/WorldTraveler/ContentView.swift
+++ WorldTraveler/WorldTraveler/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fahrenheitValue = ""
+    
+    let numberFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 2
+        return numberFormatter
+    }()
+    
+    func convertToCelsius() -> String {
+        if let value = Double(fahrenheitValue) {
+            let fahrenheit = Measurement<UnitTemperature>(value: value, unit: .fahrenheit)
+            let celsiusValue = fahrenheit.converted(to: .celsius)
+            return numberFormatter.string(from: NSNumber(value: celsiusValue.value)) ?? "???"
+        } else {
+            return "???"
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("value", text: $fahrenheitValue)
+                .multilineTextAlignment(.center)
+                .font(Font.system(size: 64.0))
+                .keyboardType(.decimalPad)
+            Text("fahrenheit")
+            Text("is actually")
+                .foregroundColor(.gray)
+            Text(convertToCelsius())
+                .font(Font.system(size: 64))
+            Text("degress Celcius")
+            Spacer()
+        }
+        .foregroundColor(.orange)
+        .font(.title)
     }
 }
 
