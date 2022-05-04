6c58efc1c23da0eb3abcd37ec64f5683ee77d77a
mehmetsagir <memetsagirint@gmail.com>
2022-05-05 01:17:23 +0300

    design map view

--- WorldTraveler/WorldTraveler/MapView.swift
+++ WorldTraveler/WorldTraveler/MapView.swift
@@ -6,10 +6,42 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    static var regions: [MKCoordinateRegion] = [
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 41.0, longitude: 29.0),
+            span: MKCoordinateSpan(latitudeDelta: 4.0, longitudeDelta: 4.0)
+        ),
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.509865, longitude: -0.118092),
+            span: MKCoordinateSpan(latitudeDelta: 4.0, longitudeDelta: 4.0)
+        ),
+    ]
+    
+    @State var region: MKCoordinateRegion = regions[0]
+    
+    
+    @State var selectedIndex = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $region)
+            .edgesIgnoringSafeArea(.top)
+            .overlay(
+                VStack {
+                    Picker("Picker", selection: $selectedIndex, content: {
+                        Text("Istanbul").tag(0)
+                        Text("London").tag(1)
+                    })
+                        .pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                        .onChange(of: selectedIndex, perform: { value in
+                            self.region = MapView.regions[selectedIndex]
+                        })
+                    Spacer()
+                }
+            )
     }
 }
 
