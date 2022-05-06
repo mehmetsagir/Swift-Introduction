aeb65c102ef6fad25d488eda067890cbe8af5bb3
mehmetsagir <memetsagirint@gmail.com>
2022-05-06 14:12:05 +0300

    add UsedGames

---
+++ UsedGames/UsedGames/UsedGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesApp.swift
+//  UsedGames
+//
+//  Created by Mehmet Sağır on 5.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
